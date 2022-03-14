845f8f3b8832d236ef118e7e395b40e01f757ee3
JohnTheBeloved <topzy20@gmail.com>
2022-03-14 04:11:30 +0100

    Update creds

--- demo/main.tf
+++ demo/main.tf
@@ -10,11 +10,20 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = "eu-west-2"
-  access_key = "AKIA4QRR65VVJ4FDIOC2"
-  secret_key = "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"
+  access_key = "AKIA4QRR65VVMDNT47KZ"
+  secret_key = "o8UFY+ceOll+InFuKy2o+T5vmkYkNRYiFN5Cw7sx"
 }
 
-# Create a VPC
-resource "aws_vpc" "example" {
-  cidr_block = "10.0.0.0/16"
+# # Create a VPC
+# resource "aws_vpc" "example" {
+#   cidr_block = "10.0.0.0/16"
+# }
+
+resource "aws_instance" "demo" {
+  ami           = "ami-0015a39e4b7c0966f"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Demo"
+  }
 }
